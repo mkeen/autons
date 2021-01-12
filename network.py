15a52ba51ec1f401ad1ce8e2ca7005632d2ab036
Mike Keen <mwk@mikekeen.com>
2021-01-12 17:37:24 -0500

    logging

--- network.py
+++ network.py
@@ -3,6 +3,17 @@ import dns.resolver
 import json
 import sys
 import random
+import logging
+import logging.handlers
+
+logger = logging.getLogger('autonslog')
+logger.setLevel(logging.DEBUG)
+handler = logging.handlers.SysLogHandler(address = '/dev/log')
+logger.addHandler(handler)
+
+DEBUG = 'debug'
+ERROR = 'error'
+FATAL = 'fatal'
 
 NS = 0
 HOST = 1
@@ -18,12 +29,25 @@ remote_providers = [
 
 random.shuffle(remote_providers)
 
+def log(message, level=DEBUG):
+	prefix = 'keen autons:'
+	if level is DEBUG:
+		return logger.debug(f"{prefix} {message}")
+	elif level is ERROR:
+		pass
+	elif level is FATAL:
+		pass
+	else
+		pass
+
 for provider in remote_providers:
 	try:
+		log(f"resolving {provider[HOST]}/{provider[REC]}")
 		resolver.nameservers=[socket.gethostbyname(provider[NS])]
 		ip_address = resolver.resolve(provider[HOST], provider[REC])[0].to_text().replace('"', '')
 		break
 	except:
+		log(f"couldn't resolve provider {provider} {sys.exc_info()[0]}", ERROR)
 		pass
 		
 if ip_address:
@@ -33,4 +57,5 @@ if ip_address:
 	}))
 	sys.exit(0)
 else:
+	log(f"couldn't contact any providers {sys.exc_info()[0]}", FATAL)
 	sys.exit(1)
